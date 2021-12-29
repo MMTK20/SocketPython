888a05bd1a3bcd9f105edcefff7db6d6e7fdca38
Cuu Quan <63086656+cuuquan2703@users.noreply.github.com>
2021-12-29 12:10:15 +0700

    utf-8 encode for JSON file

--- Server1.py
+++ Server1.py
@@ -4,6 +4,25 @@ import json
 import requests
 import codecs
 import datetime
+import re
+
+#Convert to utf-8 code
+def UTFDECODE(s):
+    s = re.sub(r'[àáạảãâầấậẩẫăằắặẳẵ]', 'a', s)
+    s = re.sub(r'[ÀÁẠẢÃĂẰẮẶẲẴÂẦẤẬẨẪ]', 'A', s)
+    s = re.sub(r'[èéẹẻẽêềếệểễ]', 'e', s)
+    s = re.sub(r'[ÈÉẸẺẼÊỀẾỆỂỄ]', 'E', s)
+    s = re.sub(r'[òóọỏõôồốộổỗơờớợởỡ]', 'o', s)
+    s = re.sub(r'[ÒÓỌỎÕÔỒỐỘỔỖƠỜỚỢỞỠ]', 'O', s)
+    s = re.sub(r'[ìíịỉĩ]', 'i', s)
+    s = re.sub(r'[ÌÍỊỈĨ]', 'I', s)
+    s = re.sub(r'[ùúụủũưừứựửữ]', 'u', s)
+    s = re.sub(r'[ƯỪỨỰỬỮÙÚỤỦŨ]', 'U', s)
+    s = re.sub(r'[ỳýỵỷỹ]', 'y', s)
+    s = re.sub(r'[ỲÝỴỶỸ]', 'Y', s)
+    s = re.sub(r'[Đ]', 'D', s)
+    s = re.sub(r'[đ]', 'd', s)
+    return s
 
 # class perpetualTimer():
 
@@ -31,23 +50,32 @@ import datetime
 
 # while True:
 #     print('a')
-    
-#Call API   
-x = requests.get("https://tygia.com/json.php")
 
-#Decode response to convert to JSON
-decoded_data = x.text.encode().decode('utf-8-sig')
-y = json.loads(decoded_data)
+RATES = []
+GOLDS = []
+def CallApi():
+   #Call API   
+   x = requests.get("https://tygia.com/json.php")
 
-#Storage JSON file
-with open('data.json','w') as outfile:
-   json.dump(y, outfile,indent=4)
+   #Decode response to convert to JSON
+   decoded_data = x.text.encode().decode('utf-8-sig')
+   y = json.loads(decoded_data)
 
-#Load JSON file
-with open('data.json') as json_file:
-   data = json.load(json_file)
+   #Storage JSON file
+   with open('data.json','w',encoding='ascii') as outfile:
+      json_file = json.dumps(y,indent=4,ensure_ascii=False)
+      outfile.write(UTFDECODE(json_file))
 
-#DO STH here
-pass
-print(type(data))
+   #Load JSON file
+   with open('data.json') as json_file:
+      data = json.load(json_file)
+
+   #DO STH here
+   global RATES 
+   RATES = data['rates']
+   global GOLDS 
+   GOLDS = data['golds']
+
+CallApi()
+
 
